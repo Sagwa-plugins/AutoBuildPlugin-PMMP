23830f3a900e39331d8e36ef35eb5354c0faa049
PresentKim <qjrmckd@gmail.com>
2018-09-22 04:44:28 +0900

    삭제 제외 배열에 없을 경우 플러그인 소스 폴더를 제거

--- src/kim/present/autobuildplugin/AutoBuildPlugin.php
+++ src/kim/present/autobuildplugin/AutoBuildPlugin.php
@@ -107,6 +107,10 @@ class AutoBuildPlugin extends plugin\PluginBase{
 			if(!$alreadyLoaded){
 				$pluginManager->loadPlugin($pharPath);
 			}
+
+			if(!in_array($pluginName, $this->getConfig()->get("remove-exceptions", []))){ //삭제 제외 배열에 없을 경우 플러그인 소스 폴더를 제거
+				Utils::removeDirectory("{$pluginDir}/");
+			}
 		}
 
 		$this->getServer()->enablePlugins(plugin\PluginLoadOrder::STARTUP);
